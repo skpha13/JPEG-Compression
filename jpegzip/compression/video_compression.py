264a8e47fc51109a9302c97d8b4c9bfd13aa5220
skpha13 <mal13adi03@gmail.com>
2024-12-31 14:05:46 +0200

    doc: added docstrings

--- jpegzip/compression/video_compression.py
+++ jpegzip/compression/video_compression.py
@@ -9,7 +9,34 @@ from compression.image_compression import ImageCompression
 
 
 class VideoCompression:
+    """A class to handle video compression by compressing each frame using the
+    ImageCompression utility and saving the compressed video.
+
+    Parameters
+    ----------
+    name : str
+        The name of the video file to be compressed, from the `input` directory.
+
+    Attributes
+    ----------
+    video : numpy.ndarray
+        A 4D numpy array representing the video frames with shape (frames, height, width, channels).
+    fps : float
+        The frames per second of the input video.
+    output_path : str
+        The file path for saving the compressed video.
+    """
+
     def __init__(self, name: str):
+        """Initializes the VideoCompression class by loading the video, extracting
+        its frames per second (fps), and setting up the output path for the compressed video.
+
+        Parameters
+        ----------
+        name : str
+            The name of the video file to be compressed that is located in the `input` directory.
+        """
+
         video, fps = load_video(name)
 
         self.video: np.ndarray = video
@@ -19,6 +46,21 @@ class VideoCompression:
         self.output_path: str = os.path.join(BASE_OUTPUT_DIR, name_compressed)
 
     def compress(self) -> float:
+        """Compresses the video frame by frame using the ImageCompression utility.
+        Saves the compressed video to the output path and calculates the average mean
+        squared error (MSE) for the compression.
+
+        Returns
+        -------
+        float
+            The average mean squared error (MSE) between the original and compressed frames.
+
+        Notes
+        -----
+        Each frame is compressed using the `ImageCompression.compress_rgb` method.
+        The video is saved in MP4 format with the codec 'mp4v'.
+        """
+
         frames, height, width, _ = self.video.shape
 
         # video codec for mp4 file
